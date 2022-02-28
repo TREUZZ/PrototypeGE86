570a99482a4567bbd402fc36517a19ed8fb47dab
NoeMook <noe.mouquet@gmail.com>
2022-02-28 15:44:02 +0100

    required

--- models/personne.py
+++ models/personne.py
@@ -4,6 +4,6 @@ from odoo import models, fields, api, exceptions
 class Personne(models.AbstractModel):
     _name = 'prototype.personne'
 
-    nom = fields.Char(string='Nom de la personne', Required="1", tracking=True)
-    prenom = fields.Char(string='Prénom de la personne', Required="1", tracking=True)
+    nom = fields.Char(string='Nom de la personne', required="1", tracking=True)
+    prenom = fields.Char(string='Prénom de la personne', required="1", tracking=True)
 
