0c68251ddfa132e4d5ec2557d9549f7fadc3f9fa
NoeMook <noe.mouquet@gmail.com>
2022-02-28 16:12:22 +0100

    correction

--- models/salarie.py
+++ models/salarie.py
@@ -4,6 +4,6 @@ class Salarie(models.Model):
     _name = 'prototype.salarie'
     _inherit = 'prototype.personne'
 
-    salaire = fields.Float(string="Salaire de la personne",required="true")
+    salaire = fields.Float(string="Salaire de la personne",required=True)
     entreprise = fields.Char(string="Nom de l'entreprise",required=True, tracking=True)
  
