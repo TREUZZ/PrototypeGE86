33d4dc06fd2f1c9a8db1235937bd9d173c6eb23f
NoeMook <noe.mouquet@gmail.com>
2022-02-28 17:19:10 +0100

    Final commit

--- models/etudiant.py
+++ models/etudiant.py
@@ -7,7 +7,16 @@ class Etudiant(models.Model):
 
     num_etu = fields.Integer(string='Numéro d\'étudiant',required=True)
     ecole = fields.Char(string='Nom de l\'école',required=True)
-    note = fields.Float(string='Note de l\'étudiant',required=True)
+    note = fields.Float(string='Note de l\'étudiant (moyenne)',required=True)
 
-    def ajoutNote(self, new_note):
-        self.note = new_note
+    def ajoutNote(self, new_note,coef):
+        '''
+        Fonction de modification de la moyenne de l'étudiant.
+        
+        :params new_note : Float : Note sur 20 de l'étudiant.
+        :params coef : Integer : Coefficient de la note.
+        '''
+        if self.note != 0:
+            self.note += coef * new_note
+        else:
+            self.note = coef * new_note
--- models/salarie.py
+++ models/salarie.py
@@ -6,4 +6,11 @@ class Salarie(models.Model):
 
     salaire = fields.Float(string="Salaire de la personne",required=True)
     entreprise = fields.Char(string="Nom de l'entreprise",required=True, tracking=True)
- 
+
+    def modificationSalaire(self,pourcentage_augmentation):
+        '''
+        Méthode de modification du salaire.
+
+        :params pourcentage_augmentation : float : Pourcentage de modification du salaire initial. 
+        '''
+        self.salaire = self.salaire * ( pourcentage_augmentation / 100 )
