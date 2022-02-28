647d33415c548cebaef1247227892c1fb68f3b51
13 <ilias.sakr@gmail.com>
2022-02-28 15:50:01 +0100

    fignoler

--- models/etudiant.py
+++ models/etudiant.py
@@ -1,11 +1,11 @@
-from odoo import model
+from odoo import model, fields
 
 class Etudiant(models.Model):
     _inherit = "personne.model"
 
-    num_etu = fields.integer(required=True)
-    ecole = fields.Char(required=True)
-    note = fields.float
+    num_etu = fields.integer(string="Nom de l'étudiant", required=True, tracking=True)
+    ecole = fields.Char(string="Nom de l'école", required=True, tracking=True)
+    note = fields.float(string="Note de l'étudiant", required=True, tracking=True)
 
     def ajoutNote(self, new_note):
         self.note = new_note
