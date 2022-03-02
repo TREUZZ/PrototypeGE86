880643ecb88d1fdc80a879acbadb52d2b782d552
13 <ilias.sakr@gmail.com>
2022-03-02 11:41:03 +0100

    update model + view

--- models/etudiant.py
+++ models/etudiant.py
@@ -1,3 +1,4 @@
+from asyncio.windows_events import NULL
 from odoo import models,fields
 
 class Etudiant(models.Model):
@@ -7,7 +8,7 @@ class Etudiant(models.Model):
 
     num_etu = fields.Integer(string='Numéro d\'étudiant',required=True)
     ecole = fields.Char(string='Nom de l\'école',required=True)
-    note = fields.Float(string='Note de l\'étudiant (moyenne)',required=True)
+    note = fields.Float(string='Note de l\'étudiant (moyenne)')
 
     def ajoutNote(self, new_note,coef):
         '''
@@ -16,7 +17,7 @@ class Etudiant(models.Model):
         :params new_note : Float : Note sur 20 de l'étudiant.
         :params coef : Integer : Coefficient de la note.
         '''
-        if self.note != 0:
+        if self.note != NULL:
             self.note += coef * new_note
         else:
             self.note = coef * new_note
